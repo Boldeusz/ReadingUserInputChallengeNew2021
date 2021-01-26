59493599ec73231656c5b93087e6acb9dd4dca70
Boldeusz <mnpower@poczta.onet.pl>
2021-01-26 18:37:16 +0100

    commit

---
+++ src/com/example/test/Main.java
@@ -0,0 +1,35 @@
+package com.example.test;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        Scanner scanner = new Scanner(System.in);
+
+
+        int x = 1;
+        int count = 0;
+        while (x <= 10) {
+            System.out.println("Enter number #" + x);
+
+            boolean hasNextInt = scanner.hasNextInt();
+            if (hasNextInt) {
+                int number = scanner.nextInt();
+                x++;
+                count += number;
+            } else {
+                System.out.println("Invalid number");
+                break;
+            }
+
+        }
+        System.out.println("Total of numbers: " + count);
+        //     int number = scanner.nextInt();
+
+        //       System.out.println("Number is: " + number);
+
+        scanner.close();
+    }
+}
